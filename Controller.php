743f47224f8653f6377df18867ae230c155456d2
Pongpayak Handsome <donus33@gmail.com>
2016-03-04 00:04:10 +0700

    update result weather in controller

--- Controller.php
+++ Controller.php
@@ -33,13 +33,14 @@
 				return json_encode($result);
 			}
 		}
-		
+
 		public function getWeater($la,$lo,$date,$tag){
 			$weatherModel = new WeatherModel;
 			$json = file_get_contents('https://ad46647d-1b77-4644-b8c6-78eed562b7a3:NAgf7Zkmlo@twcservice.au-syd.mybluemix.net:443/api/weather/v2/forecast/daily/10day?units=m&geocode=45.42%2C75.69&language=en-US');
 			$obj = json_decode($json);
 			$rain_day = array();
 			$rain_night = array();
+			$result = array();
 			$forcasts = $obj->{'forecasts'};
 			$s = 0;
 			$c = 0;
@@ -78,9 +79,25 @@
 			}
 			//calculate prop
 			//day
-			$weatherModel->getWeatherByKeys($tag,"day",$weather);
+			$rain_day_print = "";
+			foreach ($rain_day as $value) {
+				$rain_day_print = $rain_day_print . " " . $value;
+			}
+			array_push($result, "day");
+			array_push($result,'"percent"'. $s .',"date":null,"description" : ' . $weatherModel->getWeatherByKeys($tag,"day","sun").'}');
+			array_push($result,'"percent"'. $c .',"date":null,"description" : ' . $weatherModel->getWeatherByKeys($tag,"day","clear").'}');
+			array_push($result,'"percent"'. $t .',"date":'. $rain_day_print .',"description" : ' . $weatherModel->getWeatherByKeys($tag,"day","rain").'}');
 			//night
-			$weatherModel->getWeatherByKeys($tag,"night",$weather);
+			$rain_night_print = "";
+			foreach ($rain_night as $value) {
+				$rain_night_print = $rain_night_print . " " . $value;
+			}
+			array_push($result, "night");
+			array_push($result,'"percent"'. $sn .',"date":null,"description" : ' . $weatherModel->getWeatherByKeys($tag,"night","sun").'}');
+			array_push($result,'"percent"'. $cn .',"date":null,"description" : ' . $weatherModel->getWeatherByKeys($tag,"night","clear").'}');
+			array_push($result,'"percent"'. $tn .',"date":'. $rain_night_print .',"description" : ' . $weatherModel->getWeatherByKeys($tag,"night","rain").'}');
+
+			return json_encode($result);
 		}
 
 		public function insertReview(){
